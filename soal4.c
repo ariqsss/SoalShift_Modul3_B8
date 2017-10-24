8dc2d305e50594cd13b005ada087fe8ceda46abb
Unknown <akbar.exp57@gmail.com>
2017-10-24 21:04:05 +0700

    Sudah fix, cek dong

--- soal4.c
+++ soal4.c
@@ -1,23 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
-
+#include <unistd.h>
 
 pthread_t tid[100];
 //
 void *faktor(void *arg){
- int *a=(int *)arg;
+ int *a=arg;
  int b=1;
- for (int i=a ; i>=2 ; --i){
+ for (int i=*a ; i>=2 ; --i){
    b=b*i;
  }
- printf("Hasil %d! = %d\n",a,b);
+ printf("Hasil %d! = %d\n",*a,b);
 }
+
 
 int main(int argc, char *argv[]){
-for (int i=0 ; i <argc ; ++i){
-  
 
+int target[100],max=0,j;
+for (int i=1 ; i <argc ; ++i){
+ target[i]=atoi(argv[i]);
+ if(target[i]>max){ max=target[i]; j=i;}
+// //printf("%s %d\n",argv[i],target[i]);
+ pthread_create(&(tid[i]),NULL,&faktor,(void *)&target[i]);
 }
 
+for (int i=1 ; i < argc ; ++i)pthread_join(tid[i],NULL);
+
+return 0;
 }
