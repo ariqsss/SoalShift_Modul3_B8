f10aac649c6801a9312dec4ee667281bff7fd02c
ariq <ariqsudibyo21@gmail.com>
2017-10-24 18:39:02 +0700

    nambahin template fungsi untuk thread

--- soal3.c
+++ soal3.c
@@ -4,3 +4,33 @@
 #include<stdlib.h>
 #include<unistd.h>
 
+pthread_tid[5];
+
+void* kolamku(void *arg)       
+{
+   unsigned long i = 0;
+   pthread_t id = pthread_self();
+   int iter;
+   if(pthread_equal(id,tid[0]))       
+   {
+  
+   }
+   
+   else if(pthread_equal(id,tid[1]))       
+   {
+   
+   }
+   else if(pthread_equal(id,tid[2]))       
+   {   
+      }
+   else if(pthread_equal(id,tid[3]))       
+   {
+   
+     }
+   else if(pthread_equal(id,tid[4]))       
+   {
+
+   }
+   return NULL;
+}
+
