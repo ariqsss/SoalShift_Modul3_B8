eb8f2517f8bed1fdf0449d2ec74f2f91572a5235
Unknown <akbar.exp57@gmail.com>
2017-10-24 18:22:33 +0700

    Declare senajata yang dijual & fungsi beli

---
+++ soal1.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <string.h>
+
+
+char weapon[7][10];
+int value[7];
+memset(value,0,sizeof(value));
+strcpy(weapon[0],"MP4A1");
+strcpy(weapon[1],"PM2-V1");
+strcpy(weapon[2],"SPR-3");
+strcpy(weapon[3],"SS2-V5");
+strcpy(weapon[4],"SPG1-V3");
+strcpy(weapon[5],"MINE"); 
+
+
+void *beli(void *arg){
+ char x[10];
+ int y;
+ scanf("%s %d",x,&y);
+ for (int i=0 ; i < 6 ; ++i){
+  if(strcmp(weapon[i],y)==0){
+ 	value[i]+=y;
+	break;
+  }
+ }
+}
+
+int main(){
+
+
+return 0;
+}
