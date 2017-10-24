e81dcc90dd4a71e031fab26a615c89d7adcbc338
Unknown <akbar.exp57@gmail.com>
2017-10-24 20:37:24 +0700

    bikin fungsinya, aduh kok males ya , kelamaan ndak praktis :(

--- soal4.c
+++ soal4.c
@@ -1,1 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+
+pthread_t tid[100];
+
+void *faktor(void *arg){
+ int *a=(int *)arg;
+ int b=1;
+ for (int i=a ; i>=2 ; --i){
+   b=b*i;
+ }
+ printf("Hasil %d! = %d\n",a,b);
+}
+
+int main(int argc, char *argv[]){
+for (int i=0 ; i <argc ; ++i){
+  
+
+}
+
+}
