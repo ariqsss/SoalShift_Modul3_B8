6a085e4b73675444f4920f51925e7a45a4c18f27
ariq <ariqsudibyo21@gmail.com>
2017-10-24 20:32:51 +0700

    tambah template fungsi untuk thread

--- soal2.c
+++ soal2.c
@@ -6,3 +6,32 @@
 
 pthread_t tid[5];
 
+void* mine(void *arg)       
+{
+   unsigned long i = 0;
+   pthread_t id = pthread_self();
+   int iter;
+   if(pthread_equal(id,tid[0]))       
+   {
+  
+   }
+   
+   else if(pthread_equal(id,tid[1]))       
+   {
+	   
+   }
+   else if(pthread_equal(id,tid[2]))       
+   {   
+
+      }
+   else if(pthread_equal(id,tid[3]))       
+   {
+        
+     }
+   else if(pthread_equal(id,tid[4]))       
+   {    
+	
+   }
+   return NULL;
+}
+
